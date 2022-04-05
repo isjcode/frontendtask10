958914d4f041e3b84ed91d04739ead817b04442d
isjcode <98400721+isjcode@users.noreply.github.com>
2022-04-05 11:02:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const d = new Date();
+let hour = d.getHours();
+
+let tag = document.getElementsByTagName("h1")[0];
+let background = document.body.style;
+
+if (hour >= 0 && hour <= 12) {
+    tag.innerHTML = "Good Morning!";
+    background.backgroundColor = "Wheat";
+}
+
+else if (hour >= 12 && hour <= 18) {
+    tag.innerHTML = "Good Afternoon!";
+    background.backgroundColor = "Purple";
+}
+
+else if (hour >= 18 && hour <= 24) {
+    tag.innerHTML = "Good Night!";
+    background.backgroundColor = "Black";
+}
+
